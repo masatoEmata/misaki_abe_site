a31b042ff8d7e5850b7811985fb4fab09fdb63e6
Masato Emata (江俣 真人) <51349646+masatoEmata@users.noreply.github.com>
2021-04-16 13:26:51 +0900

    title description

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,23 +1,37 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import DocumentMeta from "react-document-meta";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Contact from "../components/Contact";
 
 export default function ContactScreen() {
-  document.title = "問い合わせ | 阿部美咲 Misaki Abe";
+  const meta = {
+    title: "お問い合わせ | 阿部美咲 Misaki Abe",
+    description:
+      "絵画・映像の作家、阿部美咲へのお問い合わせはこちらから。展示などの協業、購入のご相談、ご感想などお気軽にご連絡ください。",
+    canonical: "/contact/",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "react,meta,document,html,tags",
+      },
+    },
+  };
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <div class="container">
-        <Header shadow="none" />
-        <main class="mainContainer">
-          <Contact />
-        </main>
-      </div>
-      <Footer />
-    </React.Fragment>
+    <DocumentMeta {...meta}>
+      <React.Fragment>
+        <CssBaseline />
+        <div class="container">
+          <Header shadow="none" />
+          <main class="mainContainer">
+            <Contact />
+          </main>
+        </div>
+        <Footer />
+      </React.Fragment>
+    </DocumentMeta>
   );
 }
