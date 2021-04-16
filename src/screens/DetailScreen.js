13c38f47c9440f21fba83b64551d11be36298a9f
Masato Emata (江俣 真人) <51349646+masatoEmata@users.noreply.github.com>
2021-04-16 13:26:37 +0900

    title description

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -4,40 +4,54 @@ import NeuButton from "../components/NeuButton";
 import Footer from "../components/Footer";
 // import Header from "../components/Header";
 import ScrollTop from "../components/ScrollTop";
+import DocumentMeta from "react-document-meta";
 
 export default function DetailScreen() {
-  document.title = "作品詳細 | 阿部美咲 Misaki Abe";
+  const meta = {
+    title: "作品詳細 | 阿部美咲 Misaki Abe",
+    description:
+      "絵画・映像の作家、阿部美咲の作品詳細ページ。詳しい画像と説明を掲載しております。お問い合わせ・購入も受け付けています。",
+    // canonical: "http://example.com/path/to/page",
+    meta: {
+      charset: "utf-8",
+      name: {
+        keywords: "react,meta,document,html,tags",
+      },
+    },
+  };
 
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <ScrollTop />
-      {/* <Header /> */}
-      <main>
-        <div class="container">
-          <div class="detailCard">
-            <img
-              class="detailImg"
-              src="https://masatoemata.github.io/misaki-abe.github.io/common/img/work/%E4%BD%9C%E5%93%81_201207_0.jpg"
-              alt=""
-            />
-            <div class="cardContent">
-              <h3 class="threadHeading">作品タイトル</h3>
-              <p class="passage">
-                この作品の説明 この作品の説明 この作品の説明。
-              </p>
-              <p class="passage">(15号, 2021年04月制作)</p>
+    <DocumentMeta {...meta}>
+      <React.Fragment>
+        <CssBaseline />
+        <ScrollTop />
+        {/* <Header /> */}
+        <main>
+          <div class="container">
+            <div class="detailCard">
+              <img
+                class="detailImg"
+                src="https://masatoemata.github.io/misaki-abe.github.io/common/img/work/%E4%BD%9C%E5%93%81_201207_0.jpg"
+                alt=""
+              />
+              <div class="cardContent">
+                <h3 class="threadHeading">作品タイトル</h3>
+                <p class="passage">
+                  この作品の説明 この作品の説明 この作品の説明。
+                </p>
+                <p class="passage">(15号, 2021年04月制作)</p>
+              </div>
+            </div>
+            <div>
+              <NeuButton path="goBack">閉じる</NeuButton>
+              {/* <NeuButton>購入する</NeuButton>
+        <NeuButton>質問する</NeuButton> */}
+              <NeuButton path="/contact/">質問または購入する</NeuButton>
             </div>
           </div>
-          <div>
-            <NeuButton path="goBack">閉じる</NeuButton>
-            {/* <NeuButton>購入する</NeuButton>
-        <NeuButton>質問する</NeuButton> */}
-            <NeuButton path="/contact/">質問または購入する</NeuButton>
-          </div>
-        </div>
-      </main>
-      <Footer />
-    </React.Fragment>
+        </main>
+        <Footer />
+      </React.Fragment>
+    </DocumentMeta>
   );
 }
